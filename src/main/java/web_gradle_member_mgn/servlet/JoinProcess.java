21b2d89949aefe8c4b70e6b49dfee705e6f0950e
mywns123 <wnsduq2000@naver.com>
2021-04-06 16:56:44 +0900

    0406

---
+++ src/main/java/web_gradle_member_mgn/servlet/JoinProcess.java
@@ -0,0 +1,51 @@
+package web_gradle_member_mgn.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import web_gradle_member_mgn.dao.impl.MemberService;
+import web_gradle_member_mgn.dto.Member;
+
+@WebServlet("/joinProcess")
+public class JoinProcess extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private MemberService service = new MemberService();
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String id= request.getParameter("id");
+		String password= request.getParameter("password");
+		String name= request.getParameter("name");
+		int age = Integer.parseInt(request.getParameter("age"));
+		String gender= request.getParameter("gender");
+		String email= request.getParameter("email");
+		
+		Member member= new Member(id, password, name, age, gender, email);
+//		System.out.println("member :"+member);
+		service.InsertMember(member);
+		
+		HttpSession session = request.getSession();
+		Member mem =(Member)session.getAttribute("mem");
+		String s =mem.getId();
+		System.out.println("s :"+s);
+//		if(mem.getId().equals("admin")) {
+////		response.sendRedirect("memberList");
+//			request.getRequestDispatcher("memberList").forward(request, response);
+//		}
+//		request.getRequestDispatcher("loginForm.jsp").forward(request, response);
+		request.getRequestDispatcher("admin/main.jsp").forward(request, response);
+////		response.sendRedirect("main.jsp");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/admin/member_List.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원 목록</title>
+</head>
+<body>
+	<h3>회원 목록</h3>
+	<table border="1">
+		<thead>
+		<tr>
+			<td>아이디</td>			
+			<td>이름</td>
+			<td>나이</td>
+			<td>성별 </td>
+			<td>이메일주소</td>
+			<td>비고</td>
+			<td></td>			
+		</tr>
+		</thead>	
+	<tbody>
+	<c:forEach var="member" items="${list}">
+		<tr>
+			<td>${member.id}</td>			
+			<td>${member.name}</td>
+			<td>${member.age}</td>
+			<td>${member.gender} </td>
+			<td>${member.email}</td>
+			<td ><a href="memberDelList?id=${member.id}">삭제</a></td>
+			<td><a href="admin/memberUpdateList.jsp?id=${member.id}&name=${member.name}&age=${member.age}&email=${member.email}">수정하기</a></td>	
+		</tr>		
+	</c:forEach>
+	</tbody>
+	<tfoot>
+	<tr>
+	<td><a href="joinForm.jsp">추가하기</a></td>	
+	</tr>
+	</tfoot>	
+	</table>	
+</body>
+</html>
